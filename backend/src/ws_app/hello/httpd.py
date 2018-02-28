dc72078615188a977bedb33e2c16dacd2f5ca4f2
koi8-r <koi8-r@goodwin.home.lan>
2018-02-28 22:24:56 +0300

    Play with docker+aws ansible

--- backend/src/ws_app/hello/httpd.py
+++ backend/src/ws_app/hello/httpd.py
@@ -19,15 +19,24 @@ def run(this):
 
 
 httpd = _httpd.Application()
+# noinspection PyArgumentList
 httpd.route = types.MethodType(route, httpd)
+# noinspection PyArgumentList
 httpd.run = types.MethodType(run, httpd)
 
 
 @httpd.route(path='/')
-async def index(req:Request):
+async def index(req: Request):
     assert req.__class__ is Request
     return Response(text='Hello, World!')
 
+
 @httpd.route(path='/about')
-async def about(req:Request):
+async def about(req: Request):
     return Response(text='About')
+
+
+@httpd.route(path='/uuid')
+async def about(req: Request):
+    from .uuid_sinleton import _uuid
+    return Response(text=str(_uuid))
